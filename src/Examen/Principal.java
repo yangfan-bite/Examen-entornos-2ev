6119d32447fd8af53c6ec3adfd7791d3e516bb2b
Yangfan <yangfan.s318562@cesurformacion.com>
2023-03-22 16:31:09 +0100

    Segundo commit Con nuevo cambio

--- src/Examen/Principal.java
+++ src/Examen/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hola a tod@s");
+		System.out.println("Examen de ED Trimestre 2");
 	}
 
 }
