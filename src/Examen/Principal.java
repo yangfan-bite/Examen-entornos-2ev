12e114ce8a93c2ff33b3bb657328f4937c08632e
Yangfan <yangfan.s318562@cesurformacion.com>
2023-03-22 17:08:52 +0100

    Segundo cambio ramaA

--- src/Examen/Principal.java
+++ src/Examen/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
 		System.out.println("Hola a tod@s");
 		System.out.println("Examen de ED Trimestre 2");
 		System.out.println("primer cambio en la ramaA");
+		System.out.println("hasta luego");
 	}
 
 }
